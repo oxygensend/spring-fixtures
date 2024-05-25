d77d1dcbbebdcb74315b333414180025cf149d1f
oxygensend <szymon19314@wp.pl>
2024-05-25 21:20:26 +0200

    initial commit

---
+++ src/main/java/com/oxygensend/springfixtures/SpringFixturesApplication.java
@@ -0,0 +1,13 @@
+package com.oxygensend.springfixtures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFixturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFixturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oxygensend/springfixtures/SpringFixturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxygensend.springfixtures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFixturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
