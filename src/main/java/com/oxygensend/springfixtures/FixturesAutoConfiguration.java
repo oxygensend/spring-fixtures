3cbbd59a8892c2a3ff686da6256a4d3ab4bdcb43
oxygensend <szymon19314@wp.pl>
2024-05-30 08:12:58 +0200

    adjust bean conditions

--- src/main/java/com/oxygensend/springfixtures/FixturesAutoConfiguration.java
+++ src/main/java/com/oxygensend/springfixtures/FixturesAutoConfiguration.java
@@ -2,33 +2,35 @@ package com.oxygensend.springfixtures;
 
 import java.util.List;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
-@ConditionalOnProperty(name = "fixtures.enabled", havingValue = "true")
 @EnableConfigurationProperties(FixturesProperties.class)
 @AutoConfiguration
 public class FixturesAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(name = "fixtures.enabled", havingValue = "true")
     FixturesLoader fixturesLoader(List<Fixture> fixtures) {
         return new FixturesLoader(fixtures);
     }
 
     @Bean
-    @ConditionalOnProperty(name = "fixtures.trigger-event", havingValue = "APPLICATION_READY")
+    @ConditionalOnExpression("'${fixtures.enabled}' == 'true' && '${fixtures.trigger-event}' == 'APPLICATION_READY'")
     FixturesApplicationReadyListener applicationReadyListener(FixturesLoader loader) {
         return new FixturesApplicationReadyListener(loader);
     }
 
     @Bean
-    @ConditionalOnProperty(name = "fixtures.trigger-event", havingValue = "CONTEXT_REFRESHED")
+    @ConditionalOnExpression("'${fixtures.enabled}'== 'true' && '${fixtures.trigger-event}' == 'CONTEXT_REFRESHED'")
     FixturesContextRefreshedListener contextRefreshedListener(FixturesLoader loader) {
         return new FixturesContextRefreshedListener(loader);
     }
 
     @Bean
+    @ConditionalOnProperty(name = "fixtures.enabled", havingValue = "true")
     FixturesFakerProvider fixturesFakerProvider(FixturesProperties properties) {
         return new FixturesFakerProvider(properties, 100);
     }
